1a29359e05398d2ae878795a595e827db9e60cc0
kawani <192viva@gmail.com>
2023-10-30 09:05:43 +0900

    first commit

--- CALayerStudy/ViewController.swift
+++ CALayerStudy/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.layer.backgroundColor = UIColor.blue.cgColor
     }
 
+ 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var touchPointList: [CGPoint] = []
+        for point in touches {
+            touchPointList.append(point.location(in: self.view))
+        }
+        
+        guard let point = touchPointList.first else { return }
+        drawSnow(point)
+        
+    }
+}
 
+extension ViewController {
+    func drawSnow(_ point: CGPoint) {
+        let layer = CALayer()
+        let r: CGFloat = 20.0
+        let x: CGFloat = point.x - (r / 2)
+        let y: CGFloat = point.y - (r / 2)
+        layer.frame = CGRect(x: x, y: y, width: r, height: r)
+        layer.cornerRadius = r / 2
+        layer.backgroundColor = UIColor.white.cgColor
+        view.layer.addSublayer(layer)
+    }
 }
 
